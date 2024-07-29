6576b435888eeca9d63f4eddbf943494a7830f86
mohamed <mhalwasoffice@gmail.com>
2024-07-29 21:02:55 +0100

    finally

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,48 +1,60 @@
 import "./App.css";
 import { useRef, useState } from "react";
+import gsap from "gsap";
 function App() {
   const [todos, setTodos] = useState([]);
   const inputRef = useRef();
-  const handlerInputValue = () => {
+  const sectionRef = useRef(null);
+  const handleItemAddTask = () => {
     const inputValue = inputRef.current.value;
-    const newItem = {
-      competed: false,
-      task: inputValue,
-    };
-    setTodos([...todos, newItem]);
-    inputRef.current.value = "";
+    if (inputValue) {
+      const newItem = {
+        competed: false,
+        task: inputValue,
+      };
+      setTodos([...todos, newItem]);
+      const lastTask = sectionRef.current.lastChild;
+      gsap.from(lastTask, { opacity: 0, x: "-100%", ease: "back" });
+      inputRef.current.value = "";
+    }else{
+
+    }
   };
   const handleDoneItem = (index) => {
     const newTodosList = [...todos];
     newTodosList[index].competed = !newTodosList[index].competed;
     setTodos(newTodosList);
   };
-  const handleDeleteTask = (index)=>{
+  const handleDeleteTask = (index) => {
     const newTodosList = [...todos];
-    newTodosList.splice(index,1);
+    newTodosList.splice(index, 1);
     setTodos(newTodosList);
-  }
-  console.log(todos);
+  };
   return (
     <div className="App">
       <header>
         <input type="text" placeholder="Add a new task..." ref={inputRef} />
-        <button onClick={handlerInputValue}>
+        <button onClick={handleItemAddTask}>
           Create<ion-icon name="add-circle-outline"></ion-icon>
         </button>
       </header>
-      <section>
+      <section ref={sectionRef}>
         {todos.map(({ task, competed }, index) => {
           return (
             <div className="list" key={index}>
-              <div className={competed ? "TaskDone" : ''}>
+              <div className={competed ? "TaskDone" : ""}>
                 <ion-icon
                   name="checkmark-outline"
                   onClick={() => handleDoneItem(index)}
                 ></ion-icon>
                 {task}
               </div>
-              <ion-icon name="trash-outline" onClick={()=>{handleDeleteTask(index)}}></ion-icon>
+              <ion-icon
+                name="trash-outline"
+                onClick={() => {
+                  handleDeleteTask(index);
+                }}
+              ></ion-icon>
             </div>
           );
         })}
