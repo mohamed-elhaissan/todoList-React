2d5e9822db1599dcab84fa91741bca616f1f4a17
mohamed <mhalwasoffice@gmail.com>
2024-09-22 17:42:17 +0100

    updating

--- src/componants/cardContent.js
+++ src/componants/cardContent.js
@@ -2,27 +2,35 @@ import { GoPlus } from "react-icons/go";
 import moment from "moment"; // Or use date-fns
 import Item from "./items";
 import { useContext, useRef, useState } from "react";
-import { div } from "framer-motion/client"
 import { todoContext } from "../context/todolistItems";
 import { toast, ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 export default function CardContent() {
   const [currentDate, setCurrentDate] = useState(new Date());
-  
-  const {todoitem,setTodoItems} = useContext(todoContext)
-  const input = useRef()
-  const optionInput = useRef()
-  const dateInput = useRef()
 
-  const addItems = ()=>{
-    if(input.current.value.length == 0){
-      toast.error('try again')
-    }else {
-      toast.success('good')
-      console.log('task succefuly done ');
-      
+  const { todoitem, setTodoItems } = useContext(todoContext);
+  const input = useRef();
+  const optionInput = useRef();
+  const dateInput = useRef();
+
+  const addItems = () => {
+    if (input.current.value.length == 0) {
+      toast.error("try again");
+    } else {
+      const item = {
+        task: input.current.value,
+        option: optionInput.current.value,
+        datetime: dateInput.current.value,
+        color: "green",
+      };
+      setTodoItems([...todoitem, item]);
+      console.log(todoitem);
+
+      toast.success("good");
     }
-  }
-  
+  };
+
   const formattedDate = moment(currentDate).format("YYYY-MM-DD");
   const handleChange = (event) => {
     setCurrentDate(new Date(event.target.value));
@@ -30,38 +38,60 @@ export default function CardContent() {
   return (
     <div>
       <div>
-        <ToastContainer/>
+        <ToastContainer />
       </div>
       <div className="flex  relative w-[80%] font-[600]  mx-auto mb-4c gap-5  b ">
-      <input ref={input}
-        type="text" 
-        placeholder="Add a new Task"
-        className="border font-[500] rounded-lg w-[50%] px-5   focus:outline-[#6366F1]"
-      />
-      <select ref={optionInput}  className="border focus:outline-4 focus:outline-[#6366F1] rounded-lg cursor-pointer px-5">
-        <option color='blue-600' value="Work">Work</option>
-        <option color='green-600' value="Personal">Personal</option>
-        <option color='orenge-600' value="Shopping">Shopping</option>
-        <option color='red-600' value="Other">Other</option>
-      </select>
-      <div>
-        <input ref={dateInput}  value={formattedDate} onChange={handleChange}
-          type="date"
-          className="border cursor-pointer rounded-md px-4 py-2 bg-white shadow-sm focus:outline-4 focus:outline-[#6366F1]"
+        <input
+          ref={input}
+          type="text"
+          placeholder="Add a new Task"
+          className="border font-[500] rounded-lg w-[50%] px-5   focus:outline-[#6366F1]"
         />
+        <select
+          ref={optionInput}
+          className="border focus:outline-4 focus:outline-[#6366F1] rounded-lg cursor-pointer px-5"
+        >
+          <option color="blue-600" value="Work">
+            Work
+          </option>
+          <option color="green-600" value="Personal">
+            Personal
+          </option>
+          <option color="orenge-600" value="Shopping">
+            Shopping
+          </option>
+          <option color="red-600" value="Other">
+            Other
+          </option>
+        </select>
+        <div>
+          <input
+            ref={dateInput}
+            value={formattedDate}
+            onChange={handleChange}
+            type="date"
+            className="border cursor-pointer rounded-md px-4 py-2 bg-white shadow-sm focus:outline-4 focus:outline-[#6366F1]"
+          />
+        </div>
+        <button
+          onClick={addItems}
+          className="flex bg-black  focus:ring-blue-900 justify-center items-center text-white px-5 rounded-lg cursor-pointer gap-2"
+        >
+          <GoPlus />
+          Add
+        </button>
       </div>
-      <button onClick={addItems} className="flex bg-black  focus:ring-blue-900 justify-center items-center text-white px-5 rounded-lg cursor-pointer gap-2">
-        <GoPlus />
-        Add
-      </button>
-      
-    </div>
-    <div className="h-2 bg-gray-300 mx-auto w-[80%] my-2 rounded-full">
-
-    </div>
-    <div>
-      <Item />
-    </div>
+      <div className="h-2 bg-gray-300 mx-auto w-[80%] my-2 rounded-full"></div>
+      <div>
+        {todoitem?.map((item, index) => (
+          <Item
+            inputitem={item.task}
+            optionval={item.option}
+            dateval={item.datetime}
+            color={item.color}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/componants/items.js
+++ src/componants/items.js
@@ -1,17 +1,17 @@
 import { RiDeleteBinLine } from "react-icons/ri";
-export default function Item() {
+export default function Item({inputitem,optionval,dateval,color}) {
   return (
     <div className="shadow-lg mb-0 flex items-center justify-between px-4 py-5 rounded-lg mt-5 mx-auto w-1/2">
       <div className="flex items-center justify-center gap-2">
         <input type="checkbox" className="w-[100%] h-[100%]" />
-        <p>Coding</p>
+        <p>{inputitem}</p>
       </div>
       <div className="flex items-center justify-center gap-2">
         <p className="bg-green-500 font-[400] text-xs rounded-full px-3 py-1 text-white ">
-          Personal
+          {optionval}
         </p>
         <p className=" text-xs rounded-full px-2 border-2 border-[1px solid black] font-[500]">
-          20102
+          {dateval}
         </p>
         <RiDeleteBinLine className=" hover:bg-gray-100  cursor-pointer  w-[20%] h-[20%] p-2 rounded-full text-red-500 text-xl" />
       </div>
