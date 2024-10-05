4b3eff0c2d2929de7173765a089fffa59af3935a
mohamed <mhalwasoffice@gmail.com>
2024-10-05 15:43:15 +0100

    not done yet

--- src/componants/Catego.js
+++ src/componants/Catego.js
@@ -1,26 +1,43 @@
 
 import Darkmode from "./darkmode";
+import { todoContext } from "../context/todolistItems";
+
 
 import { motion } from "framer-motion";
+import { useContext, useEffect, useState } from "react";
 export default function Catego() {
+  const {todoItem} = useContext(todoContext)
+  const [completedTask,setCompletedTask] = useState(0)
+  const [activeTasks,setactiveTasks] = useState(0)
+ useEffect(()=>{
+  const completedCount = todoItem.filter((item)=>!item.completed).length 
+  
+  const activeTaskAlready = todoItem.length === 0 ?  0 : todoItem.length - completedCount
+  setCompletedTask(completedCount)
+  setactiveTasks(activeTaskAlready)
+  
+ },[todoItem])
   return (
     <div className="w-[20%] dark:bg-black dark:text-gray-400 bg-white p-4 flex flex-col justify-between h-[100vh] ">
       <div className="flex flex-col justify-start ls-start gap-2 ">
         <h1 className="font-bold text-3xl tracking-tight mb-10">Todo List</h1>
-        <FlipedLinks href="youtube.com">Home</FlipedLinks>
-        <FlipedLinks href="youtube.com">Active</FlipedLinks>
-        <FlipedLinks href="youtube.com">completed</FlipedLinks>
+        <FlipedLinks href="youtube.com" Number={todoItem.length}>Home </FlipedLinks>
+        <FlipedLinks href="youtube.com" Number={completedTask}>Active</FlipedLinks>
+        <FlipedLinks href="youtube.com" Number={activeTasks}>completed</FlipedLinks>
       </div>
       <Darkmode/>
     </div>
   );
 }
-const FlipedLinks = ({ children, href }) => {
+// this componants is for animate lol :)
+const FlipedLinks = ({ children, href,Number }) => {
   return (
-    <motion.div
+   <div   className="border border-[rgba(0,0,0,.4)] flex pl-2 p-2 rounded-md hover:bg-black hover:text-white dark:bg-gray-200  w-full "
+   >
+     <motion.div
       initial="initial"
       whileHover="hovered"
-      className="border border-[rgba(0,0,0,.4)] flex pl-2 p-2 rounded-md hover:bg-black hover:text-white dark:bg-gray-200  w-full "
+      // className="border border-[rgba(0,0,0,.4)] flex pl-2 p-2 rounded-md hover:bg-black hover:text-white dark:bg-gray-200  w-full "
     >
       <motion.a className="relative block overflow-hidden" href={href}>
         <motion.div>
@@ -65,5 +82,7 @@ const FlipedLinks = ({ children, href }) => {
         </motion.div>
       </motion.a>
     </motion.div>
+    <sub className="text-blue-400">({Number})</sub>
+   </div>
   );
 };
